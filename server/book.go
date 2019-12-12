2472c0e5f3aeabd5b4ce03a2906e9f28c9ccc3f8
joshuabezaleel <joshua.bezaleel@gmail.com>
2019-12-12 12:30:07 +0700

    CRUD handler for Book domain model

--- server/book.go
+++ server/book.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/joshuabezaleel/library-server/pkg/core/book"
@@ -23,17 +24,81 @@ func (handler *bookHandler) registerRouter(router *mux.Router) {
 }
 
 func (handler *bookHandler) createBook(w http.ResponseWriter, r *http.Request) {
+	book := book.Book{}
 
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		respondWithJSON(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	newBook, err := handler.bookService.Create(&book)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, newBook)
 }
 
 func (handler *bookHandler) getBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookID, ok := vars["bookID"]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid book ID")
+		return
+	}
 
+	newBook, err := handler.bookService.Get(bookID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, newBook)
 }
 
 func (handler *bookHandler) updateBook(w http.ResponseWriter, r *http.Request) {
+	book := book.Book{}
 
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	vars := mux.Vars(r)
+	bookID, ok := vars["bookID"]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid book ID")
+		return
+	}
+	book.ID = bookID
+
+	newBook, err := handler.bookService.Update(&book)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, newBook)
 }
 
 func (handler *bookHandler) deleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookID, ok := vars["bookID"]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid book ID")
+		return
+	}
 
+	err := handler.bookService.Delete(bookID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, "Book "+bookID+" deleted")
 }
