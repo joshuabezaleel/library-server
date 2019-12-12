f6518b3e742486acb404aa00c38795103d82cb0a
joshuabezaleel <joshua.bezaleel@gmail.com>
2019-12-12 09:57:32 +0700

    CRUD handler for User domain model.

--- server/user.go
+++ server/user.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/joshuabezaleel/library-server/pkg/core/user"
@@ -23,17 +24,82 @@ func (handler *userHandler) registerRouter(router *mux.Router) {
 }
 
 func (handler *userHandler) createUser(w http.ResponseWriter, r *http.Request) {
+	user := user.User{}
 
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	newUser, err := handler.userService.Create(&user)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, newUser)
 }
 
 func (handler *userHandler) getUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID, ok := vars["userID"]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
 
+	newUser, err := handler.userService.Get(userID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, newUser)
 }
 
 func (handler *userHandler) updateUser(w http.ResponseWriter, r *http.Request) {
+	user := user.User{}
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+
+	vars := mux.Vars(r)
+	userID, ok := vars["userID"]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
+	user.ID = userID
+
+	newUser, err := handler.userService.Update(&user)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, newUser)
 
 }
 
 func (handler *userHandler) deleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userID, ok := vars["userID"]
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID")
+		return
+	}
 
+	err := handler.userService.Delete(userID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, "User "+userID+" deleted")
 }
