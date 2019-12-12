863ee692e5a0601a1716a45ee843a747602c1da5
joshuabezaleel <joshua.bezaleel@gmail.com>
2019-12-12 19:19:08 +0700

    Add StringSlice type for slice of string for querying compatibility in Book domain model

--- pkg/core/book/book.go
+++ pkg/core/book/book.go
@@ -1,24 +1,30 @@
 package book
 
-import "time"
+import (
+	"database/sql/driver"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
 
 // Book domain model.
 type Book struct {
-	ID                string    `json:"id" db:"id"`
-	Title             string    `json:"title" db:"title"`
-	Publisher         string    `json:"publisher" db:"publisher"`
-	YearPublished     int       `json:"yearPublished" db:"year_published"`
-	CallNumber        string    `json:"callNumber" db:"call_number"`
-	CoverPicture      string    `json:"coverPicture" db:"cover_picture"`
-	ISBN              string    `json:"isbn" db:"isbn"`
-	Collation         string    `json:"collation" db:"book_collation"`
-	Edition           int       `json:"edition" db:"edition"`
-	Description       string    `json:"description" db:"description"`
-	LOCClassification string    `json:"locClassification" db:"loc_classification"`
-	Subject           []string  `json:"subject" db:"subject"`
-	Author            []string  `json:"author" db:"author"`
-	Quantity          int       `json:"quantity" db:"quantity"`
-	AddedAt           time.Time `json:"addedAt" db:"added_at"`
+	ID                string      `json:"id" db:"id"`
+	Title             string      `json:"title" db:"title"`
+	Publisher         string      `json:"publisher" db:"publisher"`
+	YearPublished     int         `json:"yearPublished" db:"year_published"`
+	CallNumber        string      `json:"callNumber" db:"call_number"`
+	CoverPicture      string      `json:"coverPicture" db:"cover_picture"`
+	ISBN              string      `json:"isbn" db:"isbn"`
+	Collation         string      `json:"collation" db:"book_collation"`
+	Edition           int         `json:"edition" db:"edition"`
+	Description       string      `json:"description" db:"description"`
+	LOCClassification string      `json:"locClassification" db:"loc_classification"`
+	Subject           StringSlice `json:"subject" db:"subject"`
+	Author            StringSlice `json:"author" db:"author"`
+	Quantity          int         `json:"quantity" db:"quantity"`
+	AddedAt           time.Time   `json:"addedAt" db:"added_at"`
 }
 
 // NewBook creates a new instance of Book domain model.
@@ -40,4 +46,19 @@ func NewBook(id string, title string, publisher string, yearPublished int, callN
 		Quantity:          quantity,
 		AddedAt:           addedAt,
 	}
+}
+
+// StringSlice is used as a slice of string to convert the particular type in every struct
+// of domain model to string for querying compatibility.
+// Inspiration: https://www.codementor.io/tamizhvendan/leveraging-interfaces-in-golang-part-2-9skptyl78
+type StringSlice []string
+
+// Value is used to convert the slice of string to joined string with "," delimiter.
+func (stringSlice StringSlice) Value() (driver.Value, error) {
+	var quotedStrings []string
+	for _, str := range stringSlice {
+		quotedStrings = append(quotedStrings, strconv.Quote(str))
+	}
+	value := fmt.Sprintf("{ %s }", strings.Join(quotedStrings, ","))
+	return value, nil
 }
