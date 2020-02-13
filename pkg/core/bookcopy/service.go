d5b7818341bf1cfa1d4c4ac1a992f6d6a7fb9b0a
joshuabezaleel <joshua.bezaleel@gmail.com>
2020-02-13 22:12:51 +0700

    Add custom error

--- pkg/core/bookcopy/service.go
+++ pkg/core/bookcopy/service.go
@@ -1,10 +1,19 @@
 package bookcopy
 
 import (
+	"errors"
 	"time"
 
 	util "github.com/joshuabezaleel/library-server/pkg"
 	"github.com/joshuabezaleel/library-server/pkg/core/book"
+)
+
+// Errors definition.
+var (
+	ErrCreateBookCopy = errors.New("Error creating Book Copy")
+	ErrGetBookCopy    = errors.New("Error retrieving Book Copy")
+	ErrUpdateBookCopy = errors.New("Error updating Book Copy")
+	ErrDeleteBookCopy = errors.New("Error deleting Book Copy")
 )
 
 // Service provides basic operations on BookCopy domain model.
@@ -41,29 +50,49 @@ func (s *service) Create(bookCopy *BookCopy) (*BookCopy, error) {
 		newBookCopy = NewBookCopy(bookCopy.ID, bookCopy.Barcode, bookCopy.BookID, bookCopy.Condition, time.Now())
 	}
 
+	newBookCopy, err := s.bookCopyRepository.Save(newBookCopy)
+	if err != nil {
+		return nil, ErrCreateBookCopy
+	}
+
 	book, err := s.bookService.Get(bookCopy.BookID)
 	if err != nil {
-		return nil, err
+		return nil, ErrGetBookCopy
 	}
 
 	book.Quantity++
 
 	_, err = s.bookService.Update(book)
 	if err != nil {
-		return nil, err
+		return nil, ErrUpdateBookCopy
 	}
 
-	return s.bookCopyRepository.Save(newBookCopy)
+	return newBookCopy, nil
 }
 
 func (s *service) Get(bookCopyID string) (*BookCopy, error) {
-	return s.bookCopyRepository.Get(bookCopyID)
+	bookCopy, err := s.bookCopyRepository.Get(bookCopyID)
+	if err != nil {
+		return nil, ErrGetBookCopy
+	}
+
+	return bookCopy, nil
 }
 
 func (s *service) Update(bookCopy *BookCopy) (*BookCopy, error) {
-	return s.bookCopyRepository.Update(bookCopy)
+	bookCopy, err := s.bookCopyRepository.Update(bookCopy)
+	if err != nil {
+		return nil, ErrUpdateBookCopy
+	}
+
+	return bookCopy, nil
 }
 
 func (s *service) Delete(bookCopyID string) error {
-	return s.bookCopyRepository.Delete(bookCopyID)
+	err := s.bookCopyRepository.Delete(bookCopyID)
+	if err != nil {
+		return ErrDeleteBookCopy
+	}
+
+	return nil
 }
